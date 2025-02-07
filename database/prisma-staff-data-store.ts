950d6ae85128542805dce1fbe21d978ca20cc9c2
chulakasam <sampathchulaka33872@gmail.com>
2025-02-07 10:26:18 +0530

    staff save,update,delete,getall done

--- database/prisma-staff-data-store.ts
+++ database/prisma-staff-data-store.ts
@@ -4,17 +4,69 @@ import Staff from "../model/Staff";
 const prisma = new PrismaClient();
 
 export async function StaffAdd(staff:Staff){
-
+   try {
+       const addedStaff = await prisma.staff.create({
+           data: {
+               staffId: staff.staffId,
+               name: staff.name,
+               position: staff.position,
+               gender: staff.gender,
+               joinedDate: staff.joinedDate,
+               dob: staff.dob,
+               contactNo: staff.contactNo,
+               email: staff.email,
+               address: staff.address,
+               LicenseNo: staff.LicenseNo
+           }
+       });
+       console.log('adding staff successfully', addedStaff);
+       return addedStaff;
+   }catch (error) {
+       console.log('error saving staff',error);
+   }
 }
 
 export async function StaffDelete(staffId:string){
+     try{
+         const deletedStaff=await prisma.staff.delete({
+             where:{staffId:staffId}
+         });
+         console.log('staff deleted successfully',deletedStaff);
+         return deletedStaff;
+     }catch (error){
+         console.log('error deleting staff',error);
+     }
 
 }
 
 export async function StaffGetAll(){
-
+   try{
+       const staffDetails = await prisma.staff.findMany();
+       return staffDetails;
+   }catch (error) {
+       console.log('error fetching staff',error);
+   }
 }
 
 export async function StaffUpdate(staffId:string,staff:Staff){
-
+    try {
+        const updatedStaff=await prisma.staff.update({
+            where:{staffId:staffId},
+            data: {
+                name: staff.name,
+                position: staff.position,
+                gender: staff.gender,
+                joinedDate: staff.joinedDate,
+                dob: staff.dob,
+                contactNo: staff.contactNo,
+                email: staff.email,
+                address: staff.address,
+                LicenseNo: staff.LicenseNo
+            }
+        });
+        console.log('staff updated successfully',updatedStaff);
+        return staff;
+    }catch (error){
+        console.log('error staff updating',error);
+    }
 }
--- routes/staff-routes.ts
+++ routes/staff-routes.ts
@@ -1,20 +1,46 @@
 import express from "express";
+import {StaffAdd, StaffDelete, StaffGetAll, StaffUpdate} from "../database/prisma-staff-data-store";
 
 const router = express.Router();
 
 router.post('/add',async (req, res) => {
-
+    const staff = req.body;
+        try{
+           const addStaff=await StaffAdd(staff);
+           res.json(addStaff);
+        }catch (error){
+            console.log('error saving staff...',error);
+        }
 });
 
 router.delete('/delete/:staffId',async (req, res) => {
-
+    const staffId = req.params.staffId;
+    try{
+        const deletedStaff=await StaffDelete(staffId);
+        res.json(deletedStaff);
+    }catch (error) {
+        console.log('error deleting staff',error);
+    }
 });
 
 router.get('/view',async (req, res) => {
-
+   try {
+       const staffDetails=await StaffGetAll();
+       res.json(staffDetails);
+   }catch (error){
+       console.log('error fetching staff details',error);
+   }
 });
 
-router.put('/update',(req, res) => {
+router.put('/update/:staffId',async (req, res) => {
+    const staffId = req.params.staffId;
+    const staff = req.body;
+    try{
+        const updateStaff=await StaffUpdate(staffId,staff);
+        res.json(updateStaff);
+    }catch (error){
+        console.log('error updating staff',error);
+    }
 
 });
 export default router;
