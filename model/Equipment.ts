6695d75461b0731e3652a85cfdd9d7fc7c1615f2
chulakasam <sampathchulaka33872@gmail.com>
2025-02-07 10:40:00 +0530

    equipment table create

---
+++ model/Equipment.ts
@@ -0,0 +1,8 @@
+export default class Equipment{
+    equipmentId!:string;
+    equipmentName!:string;
+    equipmentType!:string;
+    equipmentStatus!:string;
+    staffId!:string;
+    fieldCode!:string;
+}
